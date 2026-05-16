bb6583b1ee68d7dc0644ab6d877fb95e8cafd50a
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-05-15 21:58:10 -0400

    add interface notes

---
+++ Notes.ts
@@ -0,0 +1,67 @@
+// TypeScript have different types that need to be added to variables
+let age: number = 19;
+const username: string = "Ash-Greninja";
+console.log(age);
+console.log(username);
+
+// there is no different between float and int
+// it is all number
+let num1: number = 1;
+let num2: number = 2;
+let num3: number = 3;
+
+// there is a "any" type, basicly we can do anything with it, but usually don't use it
+let i: any = 1;
+i = "s";
+
+// functions
+// this type of function have a return type
+// the return type would be the type of the function
+function sum(a: number, b: number): number {
+	return a + b;
+}
+// this one don't have any returns, so the type is void
+function add(a: number, b: number): void {
+	console.log(a + b);
+}
+
+// ?: mark the parameter for the function to be optional
+function pokemon(name: string, type1: string, type2?: string): void {
+	console.log(`${name} is ${type1} type${type2 ? `, also ${type2} type` : ""}.`);
+}
+
+pokemon("greninja", "water", "dark");
+pokemon("pikachu", "electric");
+
+// we can set default parameters
+function time(month: string, day: number, year: number = 2026): void {
+	console.log(`Today is ${month} ${day}, ${year}`);
+}
+time("March", 6);
+
+// objects also need to declare types for each of the element
+const course: { name: string; grade: number } = { name: "english", grade: 80 };
+
+// if we need to declare the type for each object everytime we use it, it is going to take too long
+// so we can define a object as a type and reuse
+// There are 2 ways to do this
+//////////////////////////////
+// Interface
+//////////////////////////////
+// interface is used for objects that might need to be extended or implemented by a class
+// we can stack interface to add more attributes
+interface User {
+	name: string;
+	age: number;
+}
+// we can also extends one interface to make a new interface
+interface Admin extends User {
+	permissions: string[];
+}
+// with extends if User change Admin will change with it
+interface User {
+	time: number;
+}
+let Al: Admin = { name: "Al", age: 50, permissions: ["nothing"], time: 10 };
+console.log(Al);
+
